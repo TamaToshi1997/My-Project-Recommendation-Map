827a277b5daba5ddf9e9aa7a7391233ceec4d918
tamagawa <tamagawa.isys@gmail.com>
2025-01-28 12:13:50 +0900

    InputForm revise

--- src/InputForm.js
+++ src/InputForm.js
@@ -28,7 +28,7 @@ const InputForm = ({ onPlanSubmit, currentPlan }) => {
     if (!currentPlan) return;
 
     try {
-      const response = await fetch('http://localhost:5000/api/plans', {
+      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/plans`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
